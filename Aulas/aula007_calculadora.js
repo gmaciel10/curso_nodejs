7548cfd462f66c739fb89f7f9ebacd00ebb037f9
Guilherme Maciel <guilherme.maciel.9889@gmail.com>
2020-07-29 02:42:31 -0300

    Introdução ao module nodejs
    var calculadora = require('./aula007_funcoes');
    
    console.log(calculadora.soma(10,20));
    console.log(calculadora.subtrair(30,50));
    console.log(calculadora.multiplicar(10,10));
    console.log(calculadora.dividir(10,2));
    
    Nesse exemplo utilizei o require para buscar o module. Assim foi possível reutilizar a função de outra aplicação.

---
+++ Aulas/aula007_calculadora.js
@@ -0,0 +1,6 @@
+var calculadora = require('./aula007_funcoes');
+
+console.log(calculadora.soma(10,20));
+console.log(calculadora.subtrair(30,50));
+console.log(calculadora.multiplicar(10,10));
+console.log(calculadora.dividir(10,2));
