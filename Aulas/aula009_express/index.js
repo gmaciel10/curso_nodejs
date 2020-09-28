23ba6e2cf4adbf37bb8f80df3c9b3042946a7ef7
Guilherme Maciel <guilherme.maciel.9889@gmail.com>
2020-09-27 22:37:44 -0300

    Update

--- Aulas/aula009_express/index.js
+++ Aulas/aula009_express/index.js
@@ -1,2 +1,26 @@
 var express = require('express'); // importando o express
 var app = express(); // iniciando o express
+
+app.get('/:nome/:empresa', function(req,res){
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send('Olá '+nome+' da empresa '+empresa);
+});
+
+app.get('/blog/:artigo?',function(req,res){
+    var artigo = req.params.artigo;
+
+    if (artigo){
+        res.send('Artigo existe');
+    }else{
+        res.send('Artigo não existe');
+    }
+})
+
+app.listen(4000,function(erro){
+    if (erro){
+        console.log("xiii erro")
+    }else{
+        console.log('ta tudo certo')
+    }
+})
