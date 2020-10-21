b03abb25bf307d1f6210e62fb72dd02053946d46
Guilherme Maciel <guilherme.maciel.9889@gmail.com>
2020-10-20 22:02:25 -0300

    Update

--- Projetos/GuiaPerguntas/index.js
+++ Projetos/GuiaPerguntas/index.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.get('*/nome', function (req, res) { 
-    res.render('index.ejs',{
-        nome:'Guilherme',
-        linguagem: 'Java',
-        empresa: 'GuiaDoProgramador',
-        inscritos: 8000
+app.set('view engine','ejs')
+
+app.get("/:nome/:lang" ,function(req,res){
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+    var produtos = [
+        {nome:"coca-cola",preco:5},
+        {nome:"leite",preco:1.45},
+        {nome:"doritos",preco:3.14},
+        {nome:"carne",preco:15},
+        {nome:"redbul",preco:5}
+    ]
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        empresa: "Alcans",
+        inscritos: 8040,
+        msg: exibirMsg,
+        produtos:produtos
     });
-})
+});
 
-// Estou dizendo para o express utilizar o ejs como motor.
-app.set('view engine', 'ejs');
-
-app.listen(8081, function (erro) {
-    if (erro) {
-        console.log('existe um erro');
-    } else {
-        console.log('Rodando perfeitamente!');
-    }
-})
+app.listen(8082,()=> {console.log('App rodando');});
