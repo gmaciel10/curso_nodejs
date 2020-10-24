5e7d09e3cec63708f4e72766e03cae48829176b4
Guilherme Maciel <guilherme.maciel.9889@gmail.com>
2020-10-24 18:24:49 -0300

    Update
    Conectando banco mysql com Sequelize no nodeJS.

--- Projetos/GuiaPerguntas/index.js
+++ Projetos/GuiaPerguntas/index.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require('./database/database');
 
+//database conection
 
-app.set('view engine','ejs');
+connection
+    .authenticate()
+    .then(() => {
+        console.log('conexão feita com o banco de dados!')
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //bodyParser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
-app.get("/" ,function(req,res){
+app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.get('/perguntar', function(req,res){
+app.get('/perguntar', function (req, res) {
     res.render('perguntar');
 });
 
-app.post('/salvarpergunta',(req,res) =>{
+app.post('/salvarpergunta', (req, res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    res.send('formulário recebido '+ titulo + descricao);
+    res.send('formulário recebido ' + titulo + descricao);
 });
 
-app.listen(8080,()=> {console.log('App rodando');});
+app.listen(8080, () => {
+    console.log('App rodando');
+});
