16e9890804302d0ae4c8ffbc3f8d4e2c00bb7c68
Guilherme Maciel <guilherme.maciel.9889@gmail.com>
2020-10-27 21:57:44 -0300

    Update
    Projeto 'Perguntas' finalizado.

--- Projetos/GuiaPerguntas/index.js
+++ Projetos/GuiaPerguntas/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta');
 
 //database conection
 
@@ -15,7 +16,7 @@ connection
         console.log(msgErro);
     })
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //bodyParser
@@ -24,13 +25,38 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    Pergunta.findAll({raw:true}).then(perguntas =>{
-        res.render("index",{
-            perguntas:perguntas
+app.get('/', (req, res) => {
+    Pergunta.findAll({
+        raw: true,
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
         });
     });
-    
+
+});
+
+app.get('/pergunta/:id', (req, res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if (pergunta != undefined) {
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},order:[['id','DESC']]
+            }).then(respostas => {
+                res.render('pergunta', {
+                    pergunta: pergunta,
+                    respostas:respostas
+                });
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 app.get('/perguntar', function (req, res) {
@@ -43,12 +69,23 @@ app.post('/salvarpergunta', (req, res) => {
     Pergunta.create({
         titulo: titulo,
         descricao: descricao
-    }).then(()=>{
+    }).then(() => {
         res.redirect('/');
     });
 
 
 });
+
+app.post('/responder', (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect('/pergunta/' + perguntaId);
+    });
+})
 
 app.listen(8080, () => {
     console.log('App rodando');
