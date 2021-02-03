e5a9c37f23294f18ebd2d5c63e8302d4342fdf7e
Guilherme Maciel <67983309+GuilhermeMacielJs@users.noreply.github.com>
2021-02-03 01:46:26 -0300

    Update

--- Projetos/GuiaPress/categories/categoriesController.js
+++ Projetos/GuiaPress/categories/categoriesController.js
@@ -10,29 +10,111 @@ router.get('/admin/categories/new', (req, res) => {
 router.get('/admin/categories', (req, res) => {
     Category.findAll({
         order: [
-            ['id','ASC']
+            ['id', 'ASC']
         ]
     })
         .then(categories => {
             res.render('admin/categories/index.ejs', {
-                categories:categories
+                categories: categories
+            })
         })
-    })
 })
 
 router.post('/categories/save', (req, res) => {
     var title = req.body.title
     if ((title != "") && (title != undefined) && (title != null)) {
-        Category.create({
-            title: title,
-            slug: slugify(title)
-        }).then(() => {
-            res.redirect('/admin/categories/new')
+        Category.findAll({
+            where: { title: title }
+        }).then(duplicate => {
+            if (duplicate == false) {
+                Category.create({
+                    title: title,
+                    slug: slugify(title)
+                }).then(() => {
+                    res.redirect('/admin/categories/new')
+                })
+            }
+            else {
+                res.send('ja tem criado')
+            }
         })
+            .catch((erro) => {
+                res.redirect('/admin/categories')
+            })
     }
     else {
-        res.redirect('/')
-        alert('Prencha o campo da categoria')
+        res.redirect('/admin/categories')
+    }
+})
+router.post('/categories/delete', (req, res) => {
+    var id = req.body.id
+
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: { id: id }
+            }).then(() => {
+                res.redirect('/admin/categories')
+            })
+        }
+        else {
+            res.redirect('/admin/categories')
+        }
+    }
+    else {
+        res.redirect('/admin/categories')
+    }
+})
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+    var id = req.params.id
+
+    if (isNaN(id)) {
+        res.redirect('/admin/categories')
+    }
+
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render('admin/categories/edit', {
+                category: category
+            })
+        }
+        else {
+            res.redirect('/admin/categories')
+        }
+    })
+        .catch((erro) => {
+            res.redirect('/admin/categories')
+        })
+})
+
+router.post('/categories/update', (req, res) => {
+    var title = req.body.title
+    var id = req.body.id
+    if ((title != "") && (title != undefined)) {
+        Category.findAll({
+            where: { title: title }
+        }).then(duplicate => {
+            if (duplicate == false) {
+                Category.update({
+                    title: title,
+                    slug: slugify(title)
+                }, {
+                    where:{id:id}
+                }).then(() => {
+                    res.redirect('/admin/categories')
+                })
+            }
+            else {
+                res.send('ja tem criado')
+            }
+        })
+            .catch((erro) => {
+                res.redirect('/admin/categories')
+            })
+    }
+    else {
+        res.redirect('/admin/categories')
     }
 })
 
